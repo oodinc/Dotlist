8d4762382fe2c9d3994a1603b27e741a31723ec6
“oodinc” <masoodinc1@gmail.com>
2024-10-24 19:13:41 +0700

    add eng language in create edit  page

--- resources/lang/en copy/labels.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'Labels' => 'Labels', 
-    'No labels found' => 'No labels found.',
-    'View' => 'View',
-    'Edit' => 'Edit',
-    'Delete' => 'Delete',
-    'This page displays tasks grouped by their labels. Labels are displayed in capital letters' => 'This page displays tasks grouped by their labels. Labels are displayed in capital letters.',
-];
--- resources/lang/en copy/sidebar.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'today' => 'Today',
-    'priority' => 'Priority',
-    'upcoming' => 'Upcoming',
-    'labels' => 'Labels',
-    'history' => 'History',
-    'UdinTechnology. All rights reserved' => 'UdinTechnology. All rights reserved.',
-];
