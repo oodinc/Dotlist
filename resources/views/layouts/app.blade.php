a75345cf913faff4b5755614ea99ad39af14f1b3
oodinc <masoodinc1@gmail.com>
2025-03-19 16:56:12 +0700

    Update Hotjar tracking code with correct site name and ID

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,17 +30,17 @@
     </style>
     @endif
 
-        <!-- Hotjar Tracking Code for Site 5327953 (name missing) -->
-    <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:5327953,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
+<!-- Hotjar Tracking Code for Muhammad Saifuddin -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5343095,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
     </head>
   <body>
 
